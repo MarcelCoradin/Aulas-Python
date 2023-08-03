018d2b2722003b2f1dc08c1077672ed9c968e40f
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-03 02:41:29 -0300

    Create aula_31.py

---
+++ Aulas/aula_31.py
@@ -0,0 +1,12 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not  = é ou não é (tipos, valor, indentidade)
+id = indentidade
+"""
+
+v1 = 'a'
+v2 = 'b'
+
+print(id(v1))
+print(id(v2))
