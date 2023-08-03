81087959f9bd15e064dbb97b16f7a78f6dee7b69
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-03 02:39:19 -0300

    Create aula_16.py

---
+++ Aulas/aula_16.py
@@ -0,0 +1,15 @@
+# if /    elif   /else
+# se / se não se / se não
+
+entrada = input('VocÊ quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('VocÊ entro no sistema')
+
+elif entrada == 'sair':
+    print('Você saiu do sitema')
+
+else:
+    print('Você não digitou nem entrar e nem sair')
+
+print('FORA DOS BLOCOS IF, ELIF E ELSE')
