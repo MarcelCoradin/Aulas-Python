d5e909730ddf1126f63a91ee6cdac61b4260b189
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-03 02:36:06 -0300

    Create aula_4.py

---
+++ Aulas/aula_4.py
@@ -0,0 +1,17 @@
+# Tipo int e flot
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# # Positivo ou negativo. int sem sinal é considerado
+# positvo
+
+# Float -> Número com ponto flutuante
+# O tipo float respresenta qualquer número
+# Positivo ou negativo com ponto flutuantes.
+# Float sem sinal é considerado positivo.
+
+# A função type mostra o tipo que o python
+# Inferiu ao valor.
+
+print(type('Otávio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))  
