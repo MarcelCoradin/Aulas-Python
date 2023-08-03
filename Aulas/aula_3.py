25a14e9d10afb4270f0b65f1c3910b81c09ee1e9
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-03 02:36:01 -0300

    Create aula_3.py

---
+++ Aulas/aula_3.py
@@ -0,0 +1,19 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâminca / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas 
+"""
+print(1234)
+
+# Aspas Simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas Duplas
+print("Luiz Otávio")
+
+# Escape
+print(2, 'Luiz "Otávio"')
+
+# Escape
