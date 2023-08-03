d01a5118216514c390b38513c2af4ccf37aa982f
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-03 02:39:17 -0300

    Create aula_15.py

---
+++ Aulas/aula_15.py
@@ -0,0 +1,14 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+
+# Solicitta informações do usuário
+numero_1_str = input('Digite um número: ')
+numero_2_str = input('Digite outro número: ')
+
+# Converter as informações dos números para números de pontos inteiros
+numero_1_int = int(numero_1_str)
+numero_2_int = int(numero_2_str)
+
+# Saida do resultado
+print(f'A soma dos números é:{numero_1_int + numero_2_int}')
