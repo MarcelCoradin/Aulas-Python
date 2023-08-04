4bcb928ee0d20419e21653e4325c4b3a98acd946
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-04 00:39:09 -0300

    Create aula_40_exercicio.py

---
+++ Aulas/aula_40_exercicio.py
@@ -0,0 +1,19 @@
+"""
+Interando strings com while
+"""
+#.......012345678910
+nome = 'Luiz Otávio'
+
+tamanho_nome = len(nome)
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+print(novo_nome)
+
+
+
