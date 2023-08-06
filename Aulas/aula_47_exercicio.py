3707b46c1178d493a3256109089406be1df9e290
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-06 03:10:29 -0300

    Create aula_47_exercicio.py

---
+++ Aulas/aula_47_exercicio.py
@@ -0,0 +1,26 @@
+def adivinhar_palavra(palavra_secreta):
+    tentativas = 0
+    palavra_adivinhada = ['*'] * len(palavra_secreta)
+
+    print("Adivinhe a palavra secreta!")
+
+    while '*' in palavra_adivinhada:
+        letra = input("Digite uma letra: ").lower()
+        tentativas += 1
+
+        if letra in palavra_secreta:
+            for i in range(len(palavra_secreta)):
+                if palavra_secreta[i] == letra:
+                    palavra_adivinhada[i] = letra
+        else:
+            print("Letra não encontrada na palavra secreta. Tente novamente.")
+
+        print("Palavra atual:", " ".join(palavra_adivinhada))
+
+    print(f"Parabéns! Você adivinhou a palavra secreta '{palavra_secreta}' em {tentativas} tentativas.")
+
+# Palavra secreta para o jogo
+palavra_secreta = "python"
+
+# Chamar a função para iniciar o jogo
+adivinhar_palavra(palavra_secreta)
