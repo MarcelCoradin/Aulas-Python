9f4d1095343140d1426358cf55fbcec7d9facf10
Marcel Augusto Coradin <115686719+MarcelCoradin@users.noreply.github.com>
2023-08-09 03:12:18 -0300

    Create aula_48.py

---
+++ Aulas/aula_48.py
@@ -0,0 +1,19 @@
+"""
+Lista em Python
+tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimento reutilizável - índices e fatiamento
+Métodos úteis: append, insert, pop, clear, extend,+
+"""
+#         01234
+#        -54321
+string = 'ABCDE' # 5 CARACTERES (len)
+# print(bool([]))  # falsy
+# print(lista, type(lista))
+
+
+
+lista = [123, True, 'Luiz Otávio', 1.2, []]
+lista[-3] = 'Marcel'
+print(lista)
+print(lista[2], type(lista[2]))
